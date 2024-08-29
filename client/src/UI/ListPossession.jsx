6c98e5020fad0e09642705e33cbf0a07c586af9d
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-08-29 20:07:07 +0200

    feat:new button

--- client/src/UI/ListPossession.jsx
+++ client/src/UI/ListPossession.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Possession = () => {
-  return <h1>Liste de possession</h1>;
+  return <><Button variant="success" as={Link} to="/possession/create">
+    Ajouter une nouvelle possession
+  </Button></>
+    ;
 };
 
 export default Possession;
