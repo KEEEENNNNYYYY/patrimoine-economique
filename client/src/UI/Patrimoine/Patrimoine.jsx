421138bd464e48fdef05191df1dcd8609cbcbe50
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-01 13:45:19 +0200

    fix:chart bug on value changing

--- client/src/UI/Patrimoine/Patrimoine.jsx
+++ client/src/UI/Patrimoine/Patrimoine.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
-import { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
 import data from '../../data/data.json';
 import Possession from "../../models/possessions/Possession";
-import { Line } from 'react-chartjs-2';
-
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
@@ -24,28 +22,57 @@ const transformDataToPossessions = (data) => {
     )
   );
 };
+
 const Patrimoine = () => {
   const [info, setInfo] = useState([]);
-  const [dateActuelle, setDateActuelle] = useState(new Date().toISOString().split('T')[0]);
+  const [dateDebut, setDateDebut] = useState(() => localStorage.getItem('dateDebut') || new Date().toISOString().split('T')[0]);
+  const [dateFin, setDateFin] = useState(() => localStorage.getItem('dateFin') || new Date().toISOString().split('T')[0]);
 
   useEffect(() => {
     const possessions = transformDataToPossessions(data);
     setInfo(possessions);
   }, []);
 
-  const handleDateActuelleChange = (event) => {
-    setDateActuelle(event.target.value);
+  useEffect(() => {
+    localStorage.setItem('dateDebut', dateDebut);
+  }, [dateDebut]);
+
+  useEffect(() => {
+    localStorage.setItem('dateFin', dateFin);
+  }, [dateFin]);
+
+  const handleDateDebutChange = (event) => {
+    setDateDebut(event.target.value);
   };
 
-  const currentDate = new Date(dateActuelle);
+  const handleDateFinChange = (event) => {
+    setDateFin(event.target.value);
+  };
 
+  const generateDatesBetween = (startDate, endDate) => {
+    const dates = [];
+    let currentDate = new Date(startDate);
+    endDate = new Date(endDate);
+
+    while (currentDate <= endDate) {
+      dates.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dates;
+  };
+
+  const dates = generateDatesBetween(dateDebut, dateFin);
 
   const chartData = {
-    labels: info.map(pos => pos.libelle),
+    labels: dates.map(date => date.toISOString().split('T')[0]),
     datasets: [
       {
-        label: 'Valeur à la date T',
-        data: info.map(pos => pos.getValeur(currentDate)),
+        label: 'Estimation du Patrimoine',
+        data: dates.map(date =>
+          info.reduce((total, pos) => {
+            return total + pos.getValeur(date);
+          }, 0)
+        ),
         borderColor: 'rgba(75, 192, 192, 1)',
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         fill: true,
@@ -59,6 +86,7 @@ const Patrimoine = () => {
       legend: {
         position: 'top',
       },
+
       tooltip: {
         callbacks: {
           label: function (tooltipItem) {
@@ -70,27 +98,38 @@ const Patrimoine = () => {
     elements: {
       line: {
         borderWidth: 2,
+        tension: 0.5,
       },
       point: {
-        radius: 3,
+        radius: 2,
       }
     },
   };
+
   return (
     <div>
       <h1>Graphique du Patrimoine :</h1>
       <div className="chart-container">
         <Line data={chartData} options={chartOptions} />
-        <label>
-          <input
-            type="date"
-            value={dateActuelle}
-            onChange={handleDateActuelleChange}
-          />
-        </label>
+        <div>
+          <label>Date de Début:
+            <input
+              type="date"
+              value={dateDebut}
+              onChange={handleDateDebutChange}
+            />
+          </label>
+          <label>Date de Fin:
+            <input
+              type="date"
+              value={dateFin}
+              onChange={handleDateFinChange}
+            />
+          </label>
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Patrimoine;
