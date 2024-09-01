4f3bf0b331640e9ba2cc139bc875afc2572e5d97
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-01 20:52:09 +0200

    feat:designed new button on navbar

--- client/src/UI/header.jsx
+++ client/src/UI/header.jsx
@@ -5,8 +5,14 @@ import './header.css';
 const Navbar = () => {
     return (
         <nav>
-            <Link className="nav-button" to="/patrimoine">Patrimoine</Link>
-            <Link className="nav-button" to="/possession">Possession</Link>
+            <Link className="nav-home " to="/">
+                <img src="../../public/home (1).png" className='home' alt="image not found" />
+            </Link>
+            <div className="navContainer">
+                <Link className="nav-button" to="/patrimoine">Patrimoine</Link>
+                <Link className="nav-button" to="/possession">Possession</Link>
+            </div>
+
         </nav>
     );
 };
